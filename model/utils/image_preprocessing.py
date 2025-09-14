9a6d00ec03aa78a451ea790a11618a7e7a55016b
samalhitesh8-sudo <samalhitesh8@gmail.com>
2025-09-14 14:06:43 +0530

    Delete model/utils directory

--- model/utils/image_preprocessing.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from tensorflow.keras.preprocessing import image
-
-def prepare_image(img_path, target_size=(128, 128)):
-    img = image.load_img(img_path, target_size=target_size)
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
-    return img_array
